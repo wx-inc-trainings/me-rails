c01fec4eb1b328b397303c45c87e9bfe35b9d305
maxy96 <maxyespindola96@gmail.com>
2021-05-26 15:03:04 -0300

    downcase en el param de open_library

--- app/services/open_library_service.rb
+++ app/services/open_library_service.rb
@@ -5,31 +5,35 @@ class OpenLibraryService
 
   BASE_URI = OPEN_LIBRARY['base_uri']
 
-  def self.get_library(isbn)
-    response = request_to_open_library(isbn)
+  def initialize(isbn)
+    @isbn = isbn
+  end
+
+  def get_library
+    response = request_to_open_library
     if response.empty?
       { status?: :bad_request, result: { message: 'Book not found' } }
     else
-      { status?: :ok, result: format_answer_book(isbn, response) }
+      { status?: :ok, result: format_answer_book(response) }
     end
   end
 
-  def self.request_to_open_library(isbn)
-    query = {
-      bibkeys: isbn,
-      format: 'json',
-      jscmd: 'data'
-    }
-    HTTParty.get(BASE_URI, query: query)
-  end
-
-  def self.format_answer_book(isbn, response)
-    {
-      ISBN: isbn,
-      title: response.parsed_response[isbn]['title'],
-      subtitle: response.parsed_response[isbn]['subtitle'],
-      number_of_pages: response.parsed_response[isbn]['number_of_pages'],
-      authors: response.parsed_response[isbn]['authors']
-    }
-  end
+  private
+    def request_to_open_library
+      query = {
+        bibkeys: @isbn,
+        format: 'json',
+        jscmd: 'data'
+      }
+      HTTParty.get(BASE_URI, query: query)
+    end
+    def format_answer_book(response)
+      {
+        ISBN: @isbn,
+        title: response.parsed_response[@isbn]['title'],
+        subtitle: response.parsed_response[@isbn]['subtitle'],
+        number_of_pages: response.parsed_response[@isbn]['number_of_pages'],
+        authors: response.parsed_response[@isbn]['authors']
+      }
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   namespace :api do
-    resources :open_library, only: :show, param: :ISBN
+    resources :open_library, only: :show, param: :isbn
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
