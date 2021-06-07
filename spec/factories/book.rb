d0c5292d528fae9acf8d620e7964dd671cc0687f
maxy96 <maxyespindola96@gmail.com>
2021-06-07 16:43:11 -0300

    test validaciones book model

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -1,10 +1,10 @@
-FactoryBot.define do 
-    factory :book do 
-        genre { "Suspenso" }
-        author { "George Orwell" }
-        image { "portada.jpg"}
-        title { "1984" }
-        editor { "Editorial"}
-        year { "2000" }
-    end
+FactoryBot.define do
+  factory :book do
+    genre { 'Suspenso' }
+    author { 'George Orwell' }
+    image { 'portada.jpg' }
+    title { '1984' }
+    editor { 'Editorial' }
+    year { '2000' }
+  end
 end
