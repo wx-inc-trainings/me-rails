21be3540f5c19ff8733105b2891704e9b3a59ed6
maxy96 <maxyespindola96@gmail.com>
2021-06-08 15:42:11 -0300

    ofensas arregladas en book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,7 @@
 class Book < ApplicationRecord
   validates :image, :title, :editor, presence: true
 
-  validates :year, numericality: { only_integer: true}, length:{ is: 4}, presence: true
-  
-  validates :author, :genre, format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ }, presence: true
+  validates :year, numericality: { only_integer: true }, length: { is: 4 }, presence: true
 
+  validates :author, :genre, format: { with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/ }, presence: true
 end
