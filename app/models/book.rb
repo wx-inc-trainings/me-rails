f6db447a1c5f04263ebf4789f41baa710f8338c1
maxy96 <maxyespindola96@gmail.com>
2021-06-08 22:09:32 -0300

    ofensas arregladas

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,5 +3,6 @@ class Book < ApplicationRecord
 
   validates :year, numericality: { only_integer: true }, length: { is: 4 }, presence: true
 
-  validates :author, :genre, format: { with: /\A[a-zA-Z]+\W?(\s?|[a-zA-Z]+\W?)*\z/ }, presence: true
+  validates :author, :genre, format: { with: /\A[a-zA-Z]+\W?(\s?|[a-zA-Z]+\W?)*\z/ },
+                             presence: true
 end
--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -4,7 +4,7 @@ FactoryBot.define do
   factory :book do
     genre { Faker::Book.genre }
     author { Faker::Book.author }
-    image { Faker::LoremFlickr.image(size: "50x60") } #=> Url image
+    image { Faker::LoremFlickr.image(size: '50x60') } #=> Url image
     title { Faker::Book.title }
     editor { Faker::Book.publisher }
     year { Faker::Date.between(from: '1000-01-01', to: '2021-12-31').year }
