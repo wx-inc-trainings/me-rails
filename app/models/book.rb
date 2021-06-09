3f8adf585a0c4d3159ba7f6125ee51dea2992ae2
maxy96 <maxyespindola96@gmail.com>
2021-06-08 22:09:32 -0300

    cherry pick book-test faker

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,8 @@ class Book < ApplicationRecord
   validates_presence_of :genre, :image, :author, :title, :editor, :year
 
   validates_format_of :year, with: /\A\d+\z/
-  validates_format_of :author, :genre, with: /\A[a-zA-Z]+(?: [a-zA-Z]+)?\z/
 
   validates_length_of :year, maximum: 4
+  validates :author, :genre, format: { with: /\A[a-zA-Z]+\W?(\s?|[a-zA-Z]+\W?)*\z/ },
+                             presence: true
 end
