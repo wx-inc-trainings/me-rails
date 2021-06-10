1b6dbd501b589d926492973db574aa5fa5953a17
maxy96 <maxyespindola96@gmail.com>
2021-06-10 12:59:53 -0300

    add column slug en book

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_05_154144) do
+ActiveRecord::Schema.define(version: 2021_06_10_155229) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,8 @@ ActiveRecord::Schema.define(version: 2021_06_05_154144) do
     t.string "year", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "slug"
+    t.index ["slug"], name: "index_books_on_slug", unique: true
   end
 
   create_table "users", force: :cascade do |t|
