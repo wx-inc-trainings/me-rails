13b9029f2bc08a611e10f9b4a267a88b411ac053
maxy96 <maxyespindola96@gmail.com>
2021-06-10 23:10:21 -0300

    Friendly_id instalado y agregado en book

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,8 +1,15 @@
 class Book < ApplicationRecord
+  extend FriendlyId
+
+  friendly_id :title, use: :slugged
+
   validates :image, :title, :editor, presence: true
 
   validates :year, numericality: { only_integer: true }, length: { is: 4 }, presence: true
 
-  validates :author, :genre, format: { with: /\A[a-zA-Z]+\W?(\s?|[a-zA-Z]+\W?)*\z/ },
-                             presence: true
+  validates :author, :genre, format: { with: /\A[a-zA-Z]+\W?(\s?|[a-zA-Z]+\W?)*\z/ }, presence: true
+  
+  def should_generate_new_friendly_id?
+    title_changed?
+  end
 end
--- app/reducers/book_reducer.rb
+++ app/reducers/book_reducer.rb
@@ -1,9 +1,9 @@
 class BookReducer < Rack::Reducer
-    def initialize()
-        super(Book.all, 
-            -> (author:) { where('lower(author) like ?', "%#{author.downcase}%") },
-            -> (genre:) {where('lower(genre) like ?', "%#{genre.downcase}%")}, 
-            -> (title:) {where('lower(title) like ?', "%#{title.downcase}%")} 
-        )
-    end
+  def initialize
+    super(Book.all,
+        ->(author:) { where('lower(author) like ?', "%#{author.downcase}%") },
+        ->(genre:) { where('lower(genre) like ?', "%#{genre.downcase}%") },
+        ->(title:) { where('lower(title) like ?', "%#{title.downcase}%") }
+    )
+  end
 end
