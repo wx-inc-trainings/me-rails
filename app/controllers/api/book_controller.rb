480546d5ee57da9ad38a390824c12e214b9f9a9d
maxy96 <maxyespindola96@gmail.com>
2021-06-15 15:42:43 -0300

    rescue active record not found

--- app/controllers/api/book_controller.rb
+++ app/controllers/api/book_controller.rb
@@ -2,8 +2,6 @@ module Api
   class BookController < ApplicationController
     before_action :authenticate_user!
 
-    rescue_from ActiveRecord::RecordNotFound, with: :book_not_found
-
     def index
       render_paginated BookReducer.new.apply(params), each_serializer: BookSerializer
     end
@@ -11,13 +9,6 @@ module Api
     def show
       book = Book.friendly.find_by_friendly_id(params[:id])
       render json: BookSerializer.new.serialize(book).to_json
-    end
-
-    private
-
-    def book_not_found
-      e = BookError::BookNotFound.new
-      render json: Error::BookErrorSerializer.new.serialize(e), status: e.status
     end
   
   end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,12 @@ class ApplicationController < ActionController::Base
   include Wor::Paginate
   include DeviseTokenAuth::Concerns::SetUserByToken
 
+  rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+
   protect_from_forgery with: :null_session
+
+  def record_not_found(error)
+    render json: { error: error.to_s }, status: :not_found
+  end
 
 end
