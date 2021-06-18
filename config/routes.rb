d2a97a8e2e9d8f079cb10aca2a54c4d9988cefab
maxy96 <maxyespindola96@gmail.com>
2021-06-18 11:04:03 -0300

    Route create para crear book suggestion

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   namespace :api do
     resources :open_library, only: :show, param: :isbn
-    resources :book, only: [:index, :show]
+    resources :book, only: [:index, :show], param: :slug
+    resources :book_suggestion, only: [:create]
   end
 
   mount_devise_token_auth_for 'User', at: 'auth', skip: [:omniauth_callbacks]
