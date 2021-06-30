0ba86eae692e328d4dc0a609678f8130ef35a38d
maxy96 <maxyespindola96@gmail.com>
2021-06-30 16:15:51 -0300

    arreglando la serialize de book y user

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -2,7 +2,7 @@ class BookSerializer < Panko::Serializer
   attributes :author, :title, :genre, :editor, :year, :image, :times_rented, :slug
 
   def times_rented
-    object.rent.count.to_s
+    object.rents.count.to_s
   end
 
 end
