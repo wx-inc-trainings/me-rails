0f5c621d4bd0d737f1965ef2347c24d563f3a032
maxy96 <maxyespindola96@gmail.com>
2021-07-15 22:10:59 -0300

    modificando la columna titulo del modelo libro

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_183516) do
+ActiveRecord::Schema.define(version: 2021_07_16_004416) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,7 +33,7 @@ ActiveRecord::Schema.define(version: 2021_07_13_183516) do
     t.string "genre", null: false
     t.string "author", null: false
     t.string "image", null: false
-    t.string "title", null: false
+    t.string "title", limit: 25, null: false
     t.string "editor", null: false
     t.string "year", null: false
     t.datetime "created_at", precision: 6, null: false
