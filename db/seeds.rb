770387e4f4f61f6e9f7f0922a26f5ca54831489b
maxy96 <maxyespindola96@gmail.com>
2021-07-16 13:42:30 -0300

    seed book terminado

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,12 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+1000.times do 
+    Book.create(
+        genre: Faker::Book.genre, 
+        author:  Faker::Book.author, 
+        image:  Faker::LoremFlickr.image(size: '50x60'),  #=> Url image
+        title:  Faker::Book.title,
+        editor:  Faker::Book.publisher, 
+        year:  Faker::Date.between(from: '1000-01-01', to: '2021-12-31').year 
+    )
+end
